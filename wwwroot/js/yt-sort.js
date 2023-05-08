aca80fa97942c26afb530073a072f7759cfea0e9
teslabyte <15111613+teslabyte@users.noreply.github.com>
2023-05-08 14:32:09 +0200

    Implemented sorting logic, Google Identity Service auth and Youtube Data api request for updating playlist

--- wwwroot/js/yt-sort.js
+++ wwwroot/js/yt-sort.js
@@ -1,21 +1,41 @@
+
 const playlistId = "";
 const apiKey = "";
 const clientId = "";
 
-var items = [];
+const scope = 'https://www.googleapis.com/auth/youtube.force-ssl'; 
+const discoveryDocs = 'https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest';
 
-function authenticate() {
-  return gapi.auth2.getAuthInstance()
-      .signIn({scope: "https://www.googleapis.com/auth/youtube.readonly"})
-      .then(function() { console.log("Sign-in successful"); },
-            function(err) { console.error("Error signing in", err); });
+var items = [];
+var steps = [];
+
+var client;
+var access_token;
+var tokenClient;
+
+function initClient() {
+    gapi.client.init({
+        'apiKey': apiKey,
+        'clientId': clientId,
+        'scope': scope,
+        'discoveryDocs': [discoveryDocs]
+    });
 }
-function loadClient() {
-    gapi.client.setApiKey(apiKey);
-    return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
-        .then(function () { console.log("GAPI client loaded for API"); },
-            function(err) { console.error("Error loading GAPI client for API", err); });
+
+function initGis() {
+    tokenClient = google.accounts.oauth2.initTokenClient({
+        'client_id': clientId,
+        'scope': scope,
+        'callback': ''
+    });
+
+    if (gapi.client.getToken() === null) {
+        tokenClient.requestAccessToken({ prompt: 'consent' });
+    } else {
+        tokenClient.requestAccessTokken({ prompt: '' });
+    }
 }
+
 // Make sure the client is loaded and sign-in is complete before calling this method.
 function execute(nextPageToken) {
     var params = {
@@ -45,16 +65,120 @@ function execute(nextPageToken) {
               console.log("There are " + items.length + " videos in your playlist");
               let emp = "";
 
-              for (const item of items) {
-                  emp += item.snippet.title + ";";
+              let lis = longestIncreasingSub(items);
+              let sortedItems = [...items];
+
+              console.log(items.length + "  " + lis.length + "   " + sortedItems.length);
+
+              let apiCallCounter = 0;
+
+              for (let i = 0; i < items.length; i++) {
+                  for (let j = 0; j < lis.length; j++) {
+                      if (lis[j].snippet.resourceId.videoId === items[i].snippet.resourceId.videoId) {
+                          break;
+                      }
+                      else {
+                          let temp = items[i];
+
+                          oldPos = findElementPosition(sortedItems, items[i]);
+                          newPos = findElementPosition(sortedItems, lis[j]);
+
+                          if (lis[j].snippet.title > items[i].snippet.title) {
+                              newPos = oldPos < newPos ? newPos - 2 : newPos;
+
+                              ChangeElementPosition(sortedItems, oldPos, newPos);
+                              setTimeout(updatePlayListItemPosition, 1000, items[i], newPos);
+
+                              lis.splice(j, 0, temp);
+
+                              apiCallCounter++;
+                              break;
+                          }
+                          else if (j === lis.length - 1) {
+                              newPos = oldPos < newPos ? newPos : newPos + 2;
+
+                              ChangeElementPosition(sortedItems, oldPos, newPos);
+                              setTimeout(updatePlayListItemPosition, 1000, items[i], newPos);
+
+                              lis.push(temp);
+
+                              apiCallCounter++;
+                              break;
+                          }
+                      }
+                  }
               }
-              console.log(emp);              
+
+              console.log("API calls " + apiCallCounter);              
           }
             },
             function(err) { console.error("Execute error", err); });
 }
 
-gapi.load("client:auth2", function () {
-    gapi.auth2.init({ client_id: clientId });
-});
+function updatePlayListItemPosition(item, newPosition) {
+    var request = gapi.client.youtube.playlistItems.update({
+        "part": "snippet",
+        "id": item.id,
+        "snippet": {
+            "playlistId": item.snippet.playlistId,
+            "resourceId": item.snippet.resourceId,
+            "position": newPosition
+        }
+    });
+
+    request.execute(function (response) {
+        console.log(response);
+    });
+
+} 
+
+function findElementPosition(items, item) {
+    for (let i = 0; i < items.length; i++) {
+        if (items[i].snippet.resourceId.videoId == item.snippet.resourceId.videoId) return i;
+    }
+    return -1;
+}
+
+function ChangeElementPosition(items, oldPos, newPos) {
+    if (oldPos < 0 || oldPos >= items.length || newPos < 0 || newPos >= items.length) {
+        return;
+    }
+
+    const item = items[oldPos];
+    items.splice(oldPos, 1);
+    items.splice(newPos, 0, item);
+}
+
+function longestIncreasingSub(items) {
+    const n = items.length;
+    const dp = new Array(n).fill(1);
+
+    for (let i = 1; i < n; i++) {
+        for (let j = 0; j < i; j++) {
+            if (items[i].snippet.title > items[j].snippet.title) {
+                dp[i] = Math.max(dp[i], dp[j] + 1);
+            }
+        }
+    }
+
+    let maxIndex = 0;
+    for (let i = 0; i < n; i++) {
+        if (dp[i] > dp[maxIndex]) {
+            maxIndex = i;
+        }
+    }
+
+    const subsequence = [];
+    let currentLength = dp[maxIndex];
+    for (let i = maxIndex; i >= 0; i--) {
+        if (dp[i] === currentLength) {
+            subsequence.unshift(items[i]);
+            currentLength--;
+        }
+    }
+
+    return subsequence;
+}
+
+gapi.load('client', initClient);
 
